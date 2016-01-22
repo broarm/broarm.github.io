39119582909590c56b6f8c964d611a3199d38a6f
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2016-01-22 23:08:14 +0100

    finished basic page styles

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 (function(){
   'use strict';
 
-  var coverSwiper;
-  var activeSlide;
+  var coverSwiper = null,
+      gallerySwiper = null,
+      smallestImage = null,
+      activeSlide = null;
 
   $(document).ready(function() {
+
     var coverOptions = {
       speed: 300,
-      nextButton: ".swiper-button-next",
-      prevButton: ".swiper-button-prev",
+      nextButton: ".cover-button-next",
+      prevButton: ".cover-button-prev",
       loop: false,
       lazyLoading: true,
       lazyLoadingInPrevNext: true,
@@ -24,6 +27,18 @@
       }
     };
 
+    var galleryOptions = {
+      speed: 300,
+      nextButton: ".gallery-button-next",
+      prevButton: ".gallery-button-prev",
+      slidesPerView: 'auto',
+      spaceBetween: 24,
+      loop: false,
+      onInit: updateImages
+      //lazyLoading: true,
+      //lazyLoadingInPrevNext: true
+    };
+
     var cover = $(".cover");
     cover.imagesLoaded( function() {
       cover.find(".swiper-slide").focusPoint();
@@ -31,6 +46,14 @@
       cover.addClass("loaded");
     });
 
+    var gallery = $(".project-content-gallery");
+    gallery.imagesLoaded( function() {
+      gallerySwiper = new Swiper('.project-content-gallery-swiper-container', galleryOptions);
+      gallery.addClass("loaded");
+    });
+
+    $(window).on("resize", function() {updateImages(gallerySwiper)});
+    //gallerySwiper.onResize(function(gallerySwiper) {updateImages(gallerySwiper)});
 
   });
 
@@ -42,20 +65,54 @@
     var nextSlide = swiper.slides[swiper.activeIndex + 1];
     var prevSlide = swiper.slides[swiper.activeIndex - 1];
     //console.log(activeSlide);
-    if (nextSlide) $(".tooltip.swiper-button-next").attr("title", $(nextSlide).attr("title"));
-    if (prevSlide) $(".tooltip.swiper-button-prev").attr("title", $(prevSlide).attr("title"));
+    if (nextSlide) $(".tooltip.cover-button-next").attr("title", $(nextSlide).attr("title"));
+    if (prevSlide) $(".tooltip.cover-button-prev").attr("title", $(prevSlide).attr("title"));
     if (activeSlide) $(".tooltip.read-more").attr("title", "Read more about " + $(activeSlide).attr("title"));
   } //*/
 
 	/**
    * Update the interface color on slide change
-    * @param swiper
-   */
+   * @param swiper
+   * /
   function updateColor(swiper) {
     $(".site-header-person-information")
         .removeClass("dark")
         .removeClass("light")
         .addClass($(activeSlide).attr("data-image-color"));
   } //*/
+
+  function updateImages(swiper) {
+    if (!smallestImage) {
+      var prevHeight = 999999;
+      $.each(swiper.slides, function(i, slide) {
+        var currentImage = $(slide).find("img");
+        if (currentImage.height() < prevHeight) {
+          prevHeight = currentImage.height();
+          smallestImage = currentImage;
+        }
+      });
+    }
+
+    if (smallestImage) {
+      $.each(swiper.slides, function(i, slide) {
+        var currentImage = $(slide).find("img");
+        // reset the height
+        $(slide).css({
+          height: "auto",
+          width: "auto"
+        });
+
+        // calculate new width and height
+        var r = currentImage.attr("width") / currentImage.attr("height");
+        var smallestHeight = smallestImage.height();
+        $(slide).css({
+          height: smallestHeight,
+          width: (smallestHeight * r)
+        });
+
+        swiper.update();
+      });
+    }
+  }
 
 })();
